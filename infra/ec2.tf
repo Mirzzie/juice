fedd4d3ad051e88e2dc4d6badb1aeb9dd15f0c17
mirzzie <mirzad.official@outlook.com>
2025-10-03 12:27:26 +0100

    add infra

---
+++ infra/ec2.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Look up the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "juice_sg" {
+  name        = "juice-sg"
+  description = "Allow SSH and app port"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "juice" {
+  ami                    = var.ami_id
+  instance_type          = "t3.micro"
+  key_name               = "sec"
+  vpc_security_group_ids = [aws_security_group.juice_sg.id]
+  user_data              = file("user-data.sh")
+
+  tags = {
+    Name = "juice"
+  }
+}
+
+output "juice_public_ip" {
+  value = aws_instance.juice.public_ip
+}
---
+++ infra/vars.tf
@@ -0,0 +1,4 @@
+variable "ami_id" {
+  type = string
+  default = "ami-0bc691261a82b32bc"
+}
